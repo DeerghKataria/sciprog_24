a1e395c57802a421720ecb2fcec5851047fb1999
Deergh Kataria <deerghkataria@gmail.com>
2024-10-16 23:16:56 +0100

    Slight changes made to the floating points value in the Sum

--- Week2/Sum.c
+++ Week2/Sum.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 
-
 int main(void) {
 /* Declare variables */
    int i;
@@ -10,21 +9,21 @@ int main(void) {
 /* First sum */
    sum1 = 0.0;
    for (i=1; i<=1000; i++) {
-      sum1 += 1/i;
+      sum1 += 1.0/i;
    }
 
 
 /* Second sum */
    sum2 = 0.0;
    for (i=1000; i>0; i--) {
-      sum2 += 1/i;
+      sum2 += 1.0/i;
    }
 
    printf(" Sum1=%f\n",sum1);
    printf(" Sum2=%f\n",sum2);
 
 /* Find the difference */
-   diff = sum2 - sum1;
+   diff = sum1 - sum2;
 
    printf(" Difference between the two is %f\n",diff);
 
